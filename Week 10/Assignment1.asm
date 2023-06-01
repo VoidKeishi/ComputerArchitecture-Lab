988f3ffe83bd3694b8ebb25eb54ddeb81b89e5d4
VoidKeishi <phananha4@gmail.com>
2023-06-01 16:32:56 +0700

    Finish both Assignment

--- Week 10/Assignment1.asm
+++ Week 10/Assignment1.asm
@@ -7,20 +7,29 @@
 ones: .byte 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F
 
 .text   
-    li $s0, -1
+
 main:
-    addi $s0,$s0,1   
     la $t0, ones + 0($s0)
     lb $a0, 0($t0) # set value for segments  
     jal SHOW_7SEG_LEFT # show 
     nop 
+    #Delay 1s
+    jal begin_loop
+    nop
+    
     li $t3,9
     sub $s1,$t3,$s0
     la $t1, ones + 0($s1)
     lb $a0, 0($t1)  # set value for segments 
     jal SHOW_7SEG_RIGHT # show 
     nop 
+    
     bgt $s0,9,exit
+    #Delay 1s
+    jal begin_loop
+    nop
+   
+    addi $s0,$s0,1 
     j main
 exit: 
     li $v0, 10 
@@ -47,3 +56,10 @@ SHOW_7SEG_RIGHT: li $t0, SEVENSEG_RIGHT # assign port's address
     nop 
     jr $ra 
     nop
+ #Delay 1s
+begin_loop:
+    li $t0, 25000   # Calculate the number of iterations for 1 second delay
+loop:
+    addi $t0, $t0, -1   # Decrement the loop counter
+    bnez $t0, loop     # Branch back to loop if the counter is not zero
+    jr $ra
---
+++ Week 10/Assignment2.asm
@@ -0,0 +1,19 @@
+.eqv MONITOR_SCREEN 0x10010000   #Dia chi bat dau cua bo nho man hinh
+.eqv WHITE          0x00FFFFFF
+.eqv BLACK	    0x00000000
+
+.text
+initialize:
+	li $k0, MONITOR_SCREEN      #Nap dia chi bat dau cua man hinh
+	li $t0, WHITE
+	li $t1, BLACK
+	li $t2, 1
+
+loop:
+	bgt $t2,129,initialize   #For 32x32 square run in 256x512
+	sw $t1, -4($k0)
+	sw $t0, 0($k0)
+	
+	addi $k0, $k0, 4
+	addi $t2, $t2, 1
+	j loop
