b8b0086bd616bc8797100c848e3ceb810a5dcc7e
VoidKeishi <phananha4@gmail.com>
2023-07-13 19:06:31 +0700

    Finish

--- RAID_Simulation.asm
+++ RAID_Simulation.asm
@@ -4,20 +4,64 @@
     input: .space 100
     #Prompt
     prompt: .asciiz "Enter a string: "
+    error_message: .asciiz "Error: The length of input string must be a multiple of 8\nPlease enter a new string: "
+    valid: .asciiz "Valid input string\n"
     #Output
     Disk: .asciiz "     Disk 1               Disk 2               Disk 3     \n"
-    Upperline: .asciiz "---------------------------------------------------------\n"
+    Upperline: .asciiz "----------------------------------------------------------\n"
+    first_half: .space 5
+    second_half: .space 5
+    xor_part1: .space 3
+    xor_part2: .space 3
+    xor_part3: .space 3
+    xor_part4: .space 3
+    comma: .asciiz ","
+    frame_first_part_data: .asciiz "|     "
+    frame_second_part_data: .asciiz "     |"
+    space_between_column: .asciiz "     "
+    frame_first_part_xor: .asciiz "[[ "
+    frame_second_part_xor: .asciiz "]]"
 .text
-read_string:
+prompt_to_enter:
     li $v0, 4
     la $a0, prompt
     syscall
+read:
     li $v0, 8
     la $a0, input
     li $a1, 100
     syscall
-check_mul_of_8:
-#Check if the length of input string is a multiple of 8
+init_count:
     la $s0, input
+    li $s1, 0
+count:
+    #Check if the length of input string is a multiple of 8
     lb $t1, ($s0)
+    #Branch if encounter newline or null
+    beq $t1, '\n', check_mul_of_8
+    beqz $t1, check_mul_of_8
+    #Else continue counting
+    addi $s0, $s0, 1
+    addi $s1, $s1, 1
+    j count
+check_mul_of_8:
+    andi $s2, $s1, 7
+    beqz $s2, init_disk
+    j error
+init_disk:
+    li $v0, 4
+    la $a0, valid
+    syscall
+    j exit
+error:
+    li $v0, 4
+    la $a0, error_message
+    syscall
+    j read
+exit:
+    li $v0, 10
+    syscall
+
+
+    
     
--- SyntaxChecking.asm
+++ SyntaxChecking.asm
@@ -5,9 +5,9 @@
     #For storing instructions
     input: .space 100
     opcode_part: .space 10
-    operand1: .space 10
-    operand2: .space 10
-    operand3: .space 70
+    operand1: .space 30
+    operand2: .space 30
+    operand3: .space 30
     #For printing message
     prompt: .asciiz "Enter an assembly instruction(Type 'stop' if you want to exit): "
     opcode: .asciiz "Opcode '"
@@ -161,10 +161,10 @@
     "trunc.w.s","ffx","1",
     "xor","rrr","1",
     "xori","rri","1"
+    end_opcode: .space 1
     type: .asciiz "x", "r", "i", "l", "s", "f"
     register: .asciiz "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"
     float: .asciiz "$f0", "$f1", "$f2", "$f3", "$f4", "$f5", "$f6", "$f7", "$f8", "$f9", "$f10", "$f11", "$f12", "$f13", "$f14", "$f15", "$f16", "$f17", "$f18", "$f19", "$f20", "$f21", "$f22", "$f23", "$f24", "$f25", "$f26", "$f27", "$f28", "$f29", "$f30", "$f31"
-    immediate: .asciiz "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
     newline: "\n"
     space: " "
     null: "\0"
@@ -187,7 +187,7 @@ check_if_repeat:
     la $t1, stop
     #Compare strings
     loop:
-        # Load a character from user input
+        # Load a character from user input stop 
         lb $t2, ($t0)
         # Load a character from stored string "stop"
         lb $t3, ($t1)
